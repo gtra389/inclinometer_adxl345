332a3778514cd7ae5ae7cde2fe0f9aeb562d360d
gtra389 <52122223+gtra389@users.noreply.github.com>
2019-10-25 17:58:38 +0800

    Update settingRTC.ino

--- PreliminaryWork/settingRTC/settingRTC.ino
+++ PreliminaryWork/settingRTC/settingRTC.ino
@@ -11,6 +11,16 @@ RTC_DS3231 rtc;
  *  GND --> GND
  */
 
+ /*
+ *  Wire connect
+ *  DS3231 --> Leonardo
+ *  SCL --> 3
+ *  SDA --> 2
+ *  VCC --> 5V
+ *  GND --> GND
+ */
+
+
 int countNum = 0;
 void setup() {
   // Start the serial interface
@@ -27,13 +37,14 @@ void loop() {
   while (countNum == 0) {  
     tmElements_t tm;
     tm.Hour      = 17;             //set the tm structure to 23h31m30s on 13Feb2009
-    tm.Minute    = 42;
-    tm.Second    = 30;
-    tm.Day       = 28;
-    tm.Month     = 9;
+    tm.Minute    = 31;
+    tm.Second    = 00;
+    tm.Day       = 25;
+    tm.Month     = 10;
     tm.Year      = 2019 - 1970;    //tmElements_t.Year is the offset from 1970
     RTC.write(tm);                 //set the RTC from the tm structure
-    countNum ++;    
+    countNum ++;
+    Serial.println(F("Setting is done."));    
   }
   
   DateTime present = rtc.now();
